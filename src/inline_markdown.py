4cfb83ffb9e9778ae01149649e21414ef4ceb268
Alberto Benatti <albi.benni8@gmail.com>
2024-06-15 09:45:58 +0200

    finish

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -6,9 +6,19 @@ from textnode import (
     text_type_bold,
     text_type_italic,
     text_type_code,
-    text_type_link,
     text_type_image,
+    text_type_link,
 )
+
+
+def text_to_textnodes(text):
+    nodes = [TextNode(text, text_type_text)]
+    nodes = split_nodes_delimiter(nodes, "**", text_type_bold)
+    nodes = split_nodes_delimiter(nodes, "*", text_type_italic)
+    nodes = split_nodes_delimiter(nodes, "`", text_type_code)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_link(nodes)
+    return nodes
 
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
@@ -85,6 +95,7 @@ def split_nodes_link(old_nodes):
             new_nodes.append(TextNode(original_text, text_type_text))
     return new_nodes
 
+
 def extract_markdown_images(text):
     pattern = r"!\[(.*?)\]\((.*?)\)"
     matches = re.findall(pattern, text)
@@ -95,12 +106,3 @@ def extract_markdown_links(text):
     pattern = r"\[(.*?)\]\((.*?)\)"
     matches = re.findall(pattern, text)
     return matches
-
-def text_to_textnodes(text):
-    nodes = [TextNode(text, text_type_text)]
-    nodes = split_nodes_delimiter(nodes, "**", text_type_bold)
-    nodes = split_nodes_delimiter(nodes, "*", text_type_italic)
-    nodes = split_nodes_delimiter(nodes, "`", text_type_code)
-    nodes = split_nodes_image(nodes)
-    nodes = split_nodes_link(nodes)
-    return nodes
